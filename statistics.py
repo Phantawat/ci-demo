1253dbbb363d83d79131fe166ae9eb72a964552a
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-09-09 14:20:20 +0700

    Passed flake8

--- statistics.py
+++ statistics.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 def average(data):
     """Return the average of a list of numeric values in data."""
-    if len(data)==0:
+    if len(data) == 0:
         raise ValueError("List must contain at least one value")
     return sum(data)/len(data)
 
@@ -18,7 +18,7 @@ def variance(data):
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
+    :param data: list of numbers for which variance will be computed.
            Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
@@ -33,8 +33,8 @@ def variance(data):
     4.0
     """
     # ugly code.
-    n=len(data)
-    if n==0:
+    n = len(data)
+    if n == 0:
         raise ValueError("List must contain at least one value")
     avg = average(data)
     return round(sum([(x-avg)**2 for x in data])/n, 1)
@@ -43,4 +43,3 @@ def variance(data):
 def stdev(data):
     """the standard deviation of a list of values"""
     return sqrt(variance(data))
-
